5bbc2fa312f676cf1333ff0d2d7dd633b514399f
Del333ted <del333ted@protonmail.com>
2020-07-26 23:19:47 +0300

    Update

--- types/telegraf.d.ts
+++ types/telegraf.d.ts
@@ -1,3 +1,7 @@
+export interface questionOptions {
+  ttl?: number
+}
+
 declare module 'telegraf' {
   export class Context {
     question: (text: string, options?: questionOptions) => Promise<string>
