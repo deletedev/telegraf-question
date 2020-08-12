07b8628e141361cd70d847c44970c482cd1f389e
del333ted <56775051+del333ted@users.noreply.github.com>
2020-08-12 05:10:36 +0300

    Extra support

--- types/telegraf.d.ts
+++ types/telegraf.d.ts
@@ -1,9 +1,14 @@
-export interface questionOptions {
+import { ExtraReplyMessage, Message } from 'telegraf/typings/telegram-types'
+
+interface questionOptions {
   ttl?: number
+  extra: ExtraReplyMessage
 }
 
 declare module 'telegraf' {
   export class Context {
-    question: (text: string, options?: questionOptions) => Promise<string>
+    question: (text: string, options?: questionOptions) => Promise<Message>
+    setSession: any
+    getSession: an
   }
 }
